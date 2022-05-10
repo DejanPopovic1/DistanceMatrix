64f8dcf4143b01d2b56b6c967dadb3c980932f30
Dean Popovic <dejan.dp.popovic@gmail.com>
2022-05-10 23:32:02 +0200

    Add files via upload

---
+++ DistanceMatrix/Program.cs
@@ -0,0 +1,23 @@
+﻿using DistanceMatrixNamespace;
+
+class TestClass
+{
+    static void Main(string[] args)
+    {
+        List<Location> origins = new List<Location>() {
+            new Location(37.73475, -122.47523),
+            new Location(38.11751, -121.53500),
+            new Location(38.58993, -121.73333),
+        };
+        List<Location> destinations = new List<Location>() {
+            new Location(37.67184, -122.43652),
+            new Location(38.45499, -120.88909),
+            new Location(38.66347, -121.85902),
+        };
+        var distanceMatrix = DistanceMatrixCreator.GenerateDistanceMatrix(origins, destinations, "GOOGLE_MAPS_API_KEY");
+        var Origin1ToAllDestinations = distanceMatrix[0];
+        var Origin2ToAllDestinations = distanceMatrix[1];
+        var Origin3ToAllDestinations = distanceMatrix[2];
+        var Origin2ToDestination2 = distanceMatrix[1][1];
+    }
+}
